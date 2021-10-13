4eef227dd64570f686d6cd52e111770b947d4190
manojmcn <86192041+manojmcn@users.noreply.github.com>
2021-10-13 23:46:34 +0800

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello from Github"
+echo "This will test parameter passing"
+echo "Test Param: $TESTPARAM"
+echo "First Number: $FIRSTNUMBER"
+echo "Second Number: $SECONDNUMBER"
+Sum=$(($FIRSTNUMBER+$SECONDNUMBER))
+echo "SUM: $Sum"
